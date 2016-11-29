1bdb55421d6564c27237e8be3ee8bcced504ad3d
Michal <mwolodzk@gmail.com>
2016-11-29 17:24:41 +0100

    Plik z klasami programu
    Utworzyłem plik. Będą w nim znajdować się wszytkie klasy użyte w programie.

---
+++ klasy.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+
+class generator_grafow:
+
+    """
+    Generuje graf do zastosowania doświadczeniach z algorytmem symulowanego wyżarzania.
+    
+    * generuje lewy podgraf,
+    * generuje prawy podgraf,
+    * tworzy kompletną listę krawędzi między każdym z wierzchołków w podgrafie lewym i każdym wierzchołkiem w podgrafie prawym,
+    * dobiera losowe wartości wag wszystkich krawędzi,
+    """
+
+    def __init__(self):
+
+    def 
