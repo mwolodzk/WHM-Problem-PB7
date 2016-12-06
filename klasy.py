59a6c4959c3e222cd3cca5fcca4188e88612feed
Michal <mwolodzk@gmail.com>
2016-12-06 17:13:20 +0100

    Definicja części metod i atrybutów klasy grafWazony

--- klasy.py
+++ klasy.py
@@ -1,17 +1,47 @@
 #!/usr/bin/python
 
 
-class generator_grafow:
+class grafWazony:
 
     """
-    Generuje graf do zastosowania doświadczeniach z algorytmem symulowanego wyżarzania.
-    
+    Tworzy i przechowuje graf ważony do rozwiązania problemu przydziału w grafie ważonym.
+
     * generuje lewy podgraf,
-    * generuje prawy podgraf,
     * tworzy kompletną listę krawędzi między każdym z wierzchołków w podgrafie lewym i każdym wierzchołkiem w podgrafie prawym,
     * dobiera losowe wartości wag wszystkich krawędzi,
+    * posiada listę wierzchołków, jako obiekty klasy Wierzchołek lewego podgrafu
+      (https://github.com/mwolodzk/WHM-Problem-PB7/wiki/Wierzcho%C5%82ek-lewego-podgrafu),
     """
 
     def __init__(self):
-
-    def 
+        # domyślnie stwarzam graf o domyślnej liczbie wierzchołków
+        # i losowych wagach.
+        
+    def __init__(self, _liczbaLewychWierzcholkow):
+        # konstruktor grafu o podanej liczbie wierzchołków i
+        # losowych wagach.
+        
+    liczbaLewychWierzcholkow = 20   # domyśla liczebność
+    
+    def __wczytajGrafZPliku(self, sciezka):
+        """
+        Wczytanie grafu ważonego z pliku o strukturze --
+        
+        <liczba wierzchołków w lewym podgrafie> 
+           l1  l2  l3  .... ln
+        p1 w11 w12 w13 .... w1n 
+        p2 w21 w22 .....
+        ...     ........
+        pn wn1 wn2    ..... wnn 
+        
+        gdzie ln to n-ty wierzchołek z lewego podgrafu,
+              pn to n-ty wierzchołek z prawego podgrafu.
+        """
+        
+    def __generujGrafWazony(self, _liczbaLewychWierzcholkow):
+        "Generacja grafu ważonego do problemu przydziału w grafie ważonym."
+        
+    def __losujWagiWierzcholka(self, _wierzcholek):
+        "Losuje wagi dla wybranego wierzcholka."
+    
+    
